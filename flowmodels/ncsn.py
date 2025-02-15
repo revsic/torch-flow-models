cfe44cd5719dbd2ce89b76bb7532e4cbb4cb1330
revsic <revsic99@gmail.com>
2025-02-15 20:35:11 +0900

    ncsn: Inherit basis

--- flowmodels/ncsn.py
+++ flowmodels/ncsn.py
@@ -5,71 +5,98 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+from flowmodels.basis import ScoreModel, Sampler
+from flowmodels.ddpm import Scheduler
+
 
 @dataclass
-class Config:
+class NCSNScheduler(Scheduler):
     """Variance scheduler,
     Generative Modeling By Estimating Gradients of the Data Distribution, Song et al., 2019.[arXiv:1907.05600]
     """
 
-    L: int = 10
-    T: int = 100
+    vp: bool = False  # variance-exploding scheduler
+
+    T: int = 10  # denoted by `L` in Song et al., 2019.
+    R: int = 100  # denoted by `T` in Song et al., 2019.
     sigma_1: float = 1.0
     sigma_L: float = 0.01
     eps: float = 2e-5
 
-    def sigmas(self) -> list[float]:
-        factor = np.exp(np.log(self.sigma_L / self.sigma_1) / (self.L - 1))
-        return [self.sigma_1 * (factor**i) for i in range(self.L)]
+    def var(self) -> torch.Tensor:
+        factor = np.exp(np.log(self.sigma_L / self.sigma_1) / (self.T - 1))
+        # [T]
+        sigma = self.sigma_1 * (factor ** torch.arange(self.T, dtype=torch.float32))
+        # [T]
+        return sigma.square()
 
 
-class NCSN(nn.Module):
+class NCSN(nn.Module, ScoreModel):
     """Generative Modeling By Estimating Gradients of the Data Distribution, Song et al., 2019.[arXiv:1907.05600]"""
 
-    def __init__(self, module: nn.Module, config: Config):
+    def __init__(self, module: nn.Module, scheduler: Scheduler):
         super().__init__()
         self.score_estim = module
-        self.config = config
+        self.scheduler = scheduler
+        assert (
+            not self.scheduler.vp
+        ), "variance-preserving diffusion scheduler is not implemented yet."
+        self.sampler = AnnealedLangevinDynamicsSampler(scheduler)
 
     def forward(self, x_t: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         """Estimate the gradient of the log-likelihood(Stein Score) from the given x_t; t.
         Args:
             x_t: [FloatLike; [B, ...]] the given noised sample, `x_t`.
-            t: [torch.long; [B]], the current timestep of the noised sample in range[1, L].
+            t: [torch.long; [B]], the current timestep of the noised sample in range[1, T].
         Returns:
             estimated score from the given sample `x_t`.
         """
         return self.score_estim(x_t, t)
 
+    def score(self, x_t: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """Estimate the stein score from the given samples, `x_t` w.r.t. the current timesteps `t`.
+        Args:
+            x_t: [FloatLike; [B, ...]], the given samples, `x_t`.
+            t: [FloatLike; [B]], the current timesteps, in range[0, 1].
+        Returns:
+            [FloatLike; [B, ...]], the estimated stein scores.
+        """
+        # discretize in range[0, T]
+        t = (t * self.scheduler.T).long()
+        # [B, ...]
+        return self.forward(x_t, t)
+
     def loss(
         self,
         sample: torch.Tensor,
-        timesteps: torch.Tensor | None = None,
+        t: torch.Tensor | None = None,
         eps: torch.Tensor | None = None,
     ) -> torch.Tensor:
         """Compute the loss from the sample.
         Args:
-            sample: [FloatLike; [B, ...]], training data, `x_0`.
-            timesteps: [torch.long; [B]], target timesteps in range[1, L],
-                sample from uniform distribution if not provided.
-            eps: [FloatLike; [B, ...]], sample from prior distribution,
-                sample from gaussian if not provided.
+            sample: [FloatLike; [B, ...]], the training data, `x_0`.
+            t: [FloatLike; [B]], the target timesteps in range[0, 1],
+                sample from the uniform distribution if not provided.
+            eps: [FloatLike; [B, ...]], the sample from the prior distribution,
+                sample from the gaussian if not provided.
         Returns:
             [FloatLike; []], loss value.
         """
         batch_size, *_ = sample.shape
         # sample
-        if timesteps is None:
-            timesteps = torch.randint(1, self.config.L + 1, (batch_size,))
+        if t is None:
+            t = torch.rand(batch_size)
         if eps is None:
             eps = torch.randn_like(sample)
+        # discretize in range[1, T]
+        t = ((t * self.scheduler.T).long() + 1).clamp_max(self.scheduler.T)
         # compute objective
-        noised = self.noise(sample, timesteps, eps=eps)
-        estim = self.forward(noised, timesteps)
+        noised = self.noise(sample, t, eps=eps)
+        estim = self.forward(noised, t)
         # [T], zero-based
-        sigma = estim.new_tensor(self.config.sigmas())
+        sigma = self.scheduler.var().sqrt().to(estim)
         # [B, ...]
-        sigma = sigma[timesteps - 1].view([batch_size] + [1] * (sample.dim() - 1))
+        sigma = sigma[t - 1].view([batch_size] + [1] * (sample.dim() - 1))
         # [B, ...], apply `\lambda(\sigma) = \sigma^2`
         score_div_std = (noised - sample) / sigma
         return 0.5 * (sigma * estim + score_div_std).square().mean()
@@ -77,36 +104,11 @@ class NCSN(nn.Module):
     def sample(
         self,
         prior: torch.Tensor,
+        steps: int | None = None,
         verbose: Callable[[range], Iterable] | None = None,
-        eps: list[torch.Tensor] | None = None,
-    ) -> tuple[torch.Tensor, list[torch.Tensor]]:
-        """Transfer the samples from the prior distribution to the trained distribution.
-        Args:
-            prior: [FloatLike; [B, ...]], samples from the prior distribution.
-        """
-        # assign default values
-        if verbose is None:
-            verbose = lambda x: x
-        if eps is None:
-            eps = [
-                torch.randn_like(prior) for _ in range(self.config.T * self.config.L)
-            ]
-        # [L]
-        var = self.config.sigmas()
-        # loop
-        x_t, x_ts = prior, []
-        bsize, *_ = x_t.shape
-        with torch.inference_mode():
-            for i in verbose(range(self.config.L)):
-                alpha = self.config.eps * (var[i] / var[-1]) ** 2
-                for _ in verbose(range(self.config.T)):
-                    score = self.forward(
-                        x_t,
-                        torch.full((bsize,), i, dtype=torch.long),
-                    )
-                    x_t = x_t + 0.5 * alpha * score + np.sqrt(alpha) * eps[len(x_ts)]
-                    x_ts.append(x_t)
-        return x_t, x_ts
+    ) -> tuple[torch.Tensor, list[torch.Tensor]] | None:
+        """Forward to the DDPMSampler."""
+        return self.sampler.sample(self, prior, steps, verbose)
 
     def noise(
         self,
@@ -117,7 +119,7 @@ class NCSN(nn.Module):
         """Noise the given sample `x_0` to the `x_t` w.r.t. the timestep `t` and the noise `eps`.
         Args:
             x_0: [FloatLike; [B, ...]], the given sample, `x_0`.
-            t: [torch.long; [B]], the target timestep in range[1, L].
+            t: [torch.long; [B]], the target timestep in range[1, T].
             eps: [FloatLike; [B, ...]], the sample from the prior distribution.
         Returns:
             noised sample, `x_t`.
@@ -127,11 +129,62 @@ class NCSN(nn.Module):
         # assign default value
         if eps is None:
             eps = torch.randn_like(x_0)
-        # [L], zero-based
-        sigma = x_0.new_tensor(self.config.sigmas())
-        # L
-        (timesteps,) = sigma.shape
-        # [L, ...]
-        sigma = sigma.view([timesteps] + [1] * (x_0.dim() - 1))
-        # [B, ...]
+        # [T], zero-based
+        sigma = self.scheduler.var().sqrt().to(x_0)
+        # [T, ...]
+        sigma = sigma.view([self.scheduler.T] + [1] * (x_0.dim() - 1))
+        # [B, ...], variance exploding
         return x_0 + sigma[t - 1] * eps.to(x_0)
+
+
+class AnnealedLangevinDynamicsSampler(Sampler):
+    """Annealed Langevin Dynamics Sampler from NCSN,
+    Generative Modeling By Estimating Gradients of the Data Distribution, Song et al., 2019.[arXiv:1907.05600]
+    """
+
+    def __init__(self, scheduler: Scheduler):
+        self.scheduler = scheduler
+        assert (
+            not self.scheduler.vp
+        ), "varaince-preserving diffusion scheduler is not implemented yet."
+
+    def sample(
+        self,
+        model: ScoreModel,
+        prior: torch.Tensor,
+        steps: int | None = None,
+        verbose: Callable[[range], Iterable] | None = None,
+        eps: list[torch.Tensor] | None = None,
+    ) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Transfer the samples from the prior distribution to the trained distribution.
+        Args:
+            prior: [FloatLike; [B, ...]], samples from the prior distribution.
+            steps: the number of the steps.
+            verbose: whether writing the progress of the generations or not.
+        Returns:
+            [FloatLike; [B, ...]], generated samples.
+            `steps` x [FloatLike; [B, ...]], trajectories.
+        """
+        total = self.scheduler.T * self.scheduler.R
+        assert steps is None or steps == total, "unsupported steps"
+        # assign default values
+        if verbose is None:
+            verbose = lambda x: x
+        if eps is None:
+            eps = [torch.randn_like(prior) for _ in range(total)]
+        # [T]
+        var = self.scheduler.var().tolist()
+        # loop
+        x_t, x_ts = prior, []
+        bsize, *_ = x_t.shape
+        with torch.inference_mode():
+            for i in verbose(range(self.scheduler.T)):
+                alpha = self.scheduler.eps * (var[i] / max(var[-1], 1e-7))
+                for _ in verbose(range(self.scheduler.R)):
+                    score = model.score(
+                        x_t,
+                        torch.full((bsize,), i / self.scheduler.T).to(x_t),
+                    )
+                    x_t = x_t + 0.5 * alpha * score + np.sqrt(alpha) * eps[len(x_ts)]
+                    x_ts.append(x_t)
+        return x_t, x_ts
