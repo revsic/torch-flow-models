c40bebc1721a1db03ce3075fedd9ee47b2932b34
revsic <revsic99@gmail.com>
2025-02-15 21:29:39 +0900

    rf: Inherit ODE

--- flowmodels/rf.py
+++ flowmodels/rf.py
@@ -3,8 +3,10 @@ from typing import Callable, Iterable
 import torch
 import torch.nn as nn
 
+from flowmodels.ode import ODEModel, VanillaEulerSolver
 
-class RectifiedFlow(nn.Module):
+
+class RectifiedFlow(nn.Module, ODEModel):
     """
     Rectified Flow: Flow Straight and Fast: Learning to Generate and Transfer Data with Rectified Flow, Liu et al., 2022.[arXiv:2209.03003],
     Flow Matching: Flow Matching for Generative Modeling, Lipman et al., 2022.[arXiv:2210.02747]
@@ -13,6 +15,7 @@ class RectifiedFlow(nn.Module):
     def __init__(self, module: nn.Module):
         super().__init__()
         self.velocity_estim = module
+        self.solver = VanillaEulerSolver()
 
     def forward(self, x_t: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         """Estimate the causalized velocity from the given x_t; t.
@@ -24,16 +27,26 @@ class RectifiedFlow(nn.Module):
         """
         return self.velocity_estim(x_t, t)
 
+    def velocity(self, x_t: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """Estimate the velocity of the given samples, `x_t`.
+        Args:
+            x_t: [FloatLike; [B, ...]], the given samples, `x_t`.
+            t: [FloatLike; [B]], the current timesteps, in range[0, 1].
+        Returns:
+            [FloatLike; [B, ...]], the estimated velocity.
+        """
+        return self.forward(x_t, t)
+
     def loss(
         self,
         sample: torch.Tensor,
-        timesteps: torch.Tensor | None = None,
+        t: torch.Tensor | None = None,
         src: torch.Tensor | None = None,
     ) -> torch.Tensor:
         """Compute the loss from the sample.
         Args:
             sample: [FloatLike; [B, ...]], training data, `X_1`.
-            timesteps: [torch.float32; [B]], target timesteps in range[0, 1],
+            t: [torch.float32; [B]], target timesteps in range[0, 1],
                 sample from uniform distribution if not provided.
             src: [FloatLike; [B, ...]], sample from the source distribution, `X_0`,
                 sample from gaussian if not provided.
@@ -42,23 +55,24 @@ class RectifiedFlow(nn.Module):
         """
         batch_size, *_ = sample.shape
         # sample
-        if timesteps is None:
-            timesteps = torch.rand(batch_size)
+        if t is None:
+            t = torch.rand(batch_size)
         if src is None:
             src = torch.randn_like(sample)
         # compute objective
+        backup = t
         # [B, ...]
-        t = timesteps.view([batch_size] + [1] * (sample.dim() - 1))
+        t = t.view([batch_size] + [1] * (sample.dim() - 1))
         # [B, ...]
         x_t = t * sample + (1 - t) * src
         # [B, ...]
-        estim = self.forward(x_t, timesteps)
+        estim = self.forward(x_t, backup)
         return ((sample - src) - estim).square().mean()
 
     def sample(
         self,
         src: torch.Tensor,
-        steps: int = 3,
+        steps: int = 10,
         verbose: Callable[[range], Iterable] | None = None,
     ) -> tuple[torch.Tensor, list[torch.Tensor]]:
         """Transfer the samples from the prior distribution to the trained distribution, using vanilla Euler method.
@@ -66,18 +80,4 @@ class RectifiedFlow(nn.Module):
             src: [FloatLike; [B, ...]], samples from the source distribution, `X_0`.
             steps: the number of the steps.
         """
-        # assign default values
-        if verbose is None:
-            verbose = lambda x: x
-        # loop
-        x_t, x_ts = src, []
-        bsize, *_ = x_t.shape
-        with torch.inference_mode():
-            for i in verbose(range(steps)):
-                velocity = self.forward(
-                    x_t,
-                    torch.full((bsize,), i / steps, dtype=torch.float32),
-                )
-                x_t = x_t + velocity / steps
-                x_ts.append(x_t)
-        return x_t, x_ts
+        return self.solver.solve(self, src, steps, verbose)
