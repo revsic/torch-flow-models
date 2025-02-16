afee51742ff4e8d408cf715ac9b226577850f5a6
revsic <revsic99@gmail.com>
2025-02-16 18:47:30 +0900

    instaflow: Support instaflow

--- flowmodels/__init__.py
+++ flowmodels/__init__.py
@@ -1,6 +1,7 @@
 from flowmodels.ddim import DDIMSampler, DDIMScheduler
 from flowmodels.ddpm import DDPM, DDPMSampler, DDPMScheduler
 from flowmodels.euler import VanillaEulerSolver
+from flowmodels.instaflow import InstaFlow
 from flowmodels.ncsn import AnnealedLangevinDynamicsSampler, NCSN, NCSNScheduler
 from flowmodels.pfode import ProbabilityFlowODESampler
 from flowmodels.rf import RectifiedFlow
---
+++ flowmodels/instaflow.py
@@ -0,0 +1,89 @@
+from typing import Callable, Iterable
+
+import torch
+import torch.nn.functional as F
+
+from flowmodels.basis import ContinuousScheduler, ScoreModel, Scheduler
+from flowmodels.rf import RectifiedFlow
+from flowmodels.pfode import ProbabilityFlowODESampler
+
+
+class InstaFlow(RectifiedFlow):
+    """InstaFlow: One Step is Enough For High-Quality Diffusion-Based Text-to-Image Generation, Liu et al., ICLR 2024.[arXiv:2309.06380]"""
+
+    def distill_from_score(
+        self,
+        score_model: ScoreModel,
+        scheduler: Scheduler | ContinuousScheduler,
+        optim: torch.optim.Optimizer,
+        prior: torch.Tensor,
+        training_steps: int,
+        batch_size: int,
+        steps: int | None = None,
+        verbose: Callable[[range], Iterable] | None = None,
+    ) -> tuple[list[float], torch.Tensor]:
+        """Distill the knowledge from the given score model.
+        Args:
+            score_model: the target score model.
+            scheduler: the noise scheduler for sampling score model.
+            optim: optimizer constructed with `self.parameters()`.
+            prior: [FloatLike; [D, ...]], samples from the prior distribution.
+            training_steps: the number of the training steps.
+            batch_size: the size of the batch.
+            steps: the number of the sampling iterations (for score model).
+        """
+        sampler = ProbabilityFlowODESampler(scheduler)
+        sample, _ = sampler.sample(
+            model=score_model, prior=prior, steps=steps, verbose=verbose
+        )
+        losses = super().reflow(
+            optim=optim,
+            src=prior,
+            training_steps=training_steps,
+            batch_size=batch_size,
+            sample=sample,
+            verbose=verbose,
+        )
+        return losses, sample
+
+    def distillation(
+        self,
+        optim: torch.optim.Optimizer,
+        src: torch.Tensor,
+        training_steps: int,
+        batch_size: int,
+        sample: torch.Tensor | int = 1000,
+        loss_fn: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = F.mse_loss,
+        verbose: Callable[[range], Iterable] | None = None,
+    ):
+        """Distillation for single-step generation.
+        Args:
+            optim: optimizer constructed with `self.parameters()`.
+            src: [FloatLike; [D, ...]], samples from the source distribution, `X_0`.
+            training_steps: the number of the training steps.
+            batch_size: the size of the batch.
+            sample: [FloatLike; [D, ...]], corresponding samples transfered from `src`,
+                sample just-in-time if given is integer (assume as the number of the steps for sampling iterations).
+            loss_fn: similarity measure between the sample and single-step generated one.
+        """
+        if isinstance(sample, int):
+            with torch.inference_mode():
+                sample, _ = self.sample(src, sample, verbose)
+
+        t = torch.zeros(batch_size, dtype=sample.dtype, device=sample.device)
+
+        losses = []
+        self.train()
+        for i in verbose(range(training_steps)):
+            indices = torch.randint(0, len(sample), (batch_size,))
+            # [B, ...]
+            estim = src[indices] + self.forward(src[indices], t)
+            loss = loss_fn(sample[indices], estim)
+            # update
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            # log
+            losses.append(loss.detach().item())
+
+        return losses
