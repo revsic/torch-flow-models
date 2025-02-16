cdfe78a55ce35f63b246d3543820d248fe8dec72
revsic <revsic99@gmail.com>
2025-02-16 13:14:19 +0900

    pfode: Support PF ODE Solver

---
+++ flowmodels/pfode.py
@@ -0,0 +1,134 @@
+from typing import Callable, Iterable
+
+import torch
+import torch.nn.functional as F
+
+from flowmodels.basis import Scheduler, ScoreModel
+from flowmodels.ode import ODESolver
+
+
+class DiscretizedProbabilityFlowODESolver(ODESolver):
+    """Discretized probability flow ODE (ancestral) solver for time-discretized score model.
+    Score-Based Generative Modeling Through Stochastic Differential Equations, Song et al., 2021.[arXiv:2011.13456]
+    """
+
+    def __init__(self, scheduler: Scheduler):
+        self.scheduler = scheduler
+
+    def solve(
+        self,
+        model: ScoreModel,
+        init: torch.Tensor,
+        steps: int | None = None,
+        verbose: Callable[[range], Iterable] | None = None,
+    ) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Solve the ODE defined in the range of t; [0, 1].
+        Args:
+            model: the discretized score estimation model.
+            init: [FloatLike; [B, ...]], starting point of the ODE.
+            steps: the number of the steps, default 100 iterations.
+            verbose: whether writing the progress of the generations or not.
+        Returns:
+            [FloatLike; [B, ...]], the solution.
+            `steps` x [FloatLike; [B, ...]], trajectories.
+        """
+        # variance preserving
+        if self.scheduler.vp:
+            return self._solve_variance_preserving_score_model(
+                model,
+                init,
+                steps,
+                verbose,
+            )
+        # variance exploding
+        return self._solve_variance_exploding_score_model(
+            model,
+            init,
+            steps,
+            verbose,
+        )
+
+    def _solve_variance_preserving_score_model(
+        self,
+        model: ScoreModel,
+        init: torch.Tensor,
+        steps: int | None = None,
+        verbose: Callable[[range], Iterable] | None = None,
+    ) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Solve the ODE defined in the range of t; [0, 1].
+        Args:
+            model: the discretized time-preserving score estimation model.
+            init: [FloatLike; [B, ...]], starting point of the ODE.
+            steps: the number of the steps, default 100 iterations.
+            verbose: whether writing the progress of the generations or not.
+        Returns:
+            [FloatLike; [B, ...]], the solution.
+            `steps` x [FloatLike; [B, ...]], trajectories.
+        """
+        assert self.scheduler.vp, "unsupported scheduler; variance-exploding scheduler"
+        assert steps is None or steps == self.scheduler.T, "unsupported steps"
+        # assign default values
+        if verbose is None:
+            verbose = lambda x: x
+        # B
+        bsize, *_ = init.shape
+        # [T, ...]
+        alpha_bar = (1 - self.scheduler.var()).view(
+            [self.scheduler.T] + [1] * (init.dim() - 1)
+        )
+        # [T, ...]
+        beta = 1 - alpha_bar / F.pad(
+            alpha_bar[:-1], [0, 0] * (init.dim() - 1) + [1, 0], "constant", 1.0
+        )
+        x_t, x_ts = init, []
+        with torch.inference_mode():
+            for t in verbose(range(self.scheduler.T, 0, -1)):
+                score = model.score(
+                    x_t,
+                    torch.full((bsize,), t / self.scheduler.T),
+                )
+                b = beta[t - 1, None]
+                x_t = (2 - (1 - b).sqrt().to(x_t)) * x_t + 0.5 * b.to(x_t) * score
+                x_ts.append(x_t)
+        return x_t, x_ts
+
+    def _solve_variance_exploding_score_model(
+        self,
+        model: ScoreModel,
+        init: torch.Tensor,
+        steps: int | None = None,
+        verbose: Callable[[range], Iterable] | None = None,
+    ) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Solve the ODE defined in the range of t; [0, 1].
+        Args:
+            model: the discretized variance-exploding score estimation model.
+            init: [FloatLike; [B, ...]], starting point of the ODE.
+            steps: the number of the steps, default 100 iterations.
+            verbose: whether writing the progress of the generations or not.
+        Returns:
+            [FloatLike; [B, ...]], the solution.
+            `steps` x [FloatLike; [B, ...]], trajectories.
+        """
+        assert (
+            not self.scheduler.vp
+        ), "unsupported scheduler; variance-preserving scheduler"
+        assert steps is None or steps == self.scheduler.T, "unsupported steps"
+        # assign default values
+        if verbose is None:
+            verbose = lambda x: x
+        # B
+        bsize, *_ = init.shape
+        # [T + 1, ...]
+        var = F.pad(self.scheduler.var(), [1, 0], "constant", 0.0).view(
+            [self.scheduler.T + 1] + [1] * (init.dim() - 1)
+        )
+        x_t, x_ts = init, []
+        with torch.inference_mode():
+            for t in verbose(range(self.scheduler.T, 0, -1)):
+                score = model.score(
+                    x_t,
+                    torch.full((bsize,), t / self.scheduler.T),
+                )
+                x_t = x_t + 0.5 * (var[t - 1] - var[t - 2]).to(score) * score
+                x_ts.append(x_t)
+        return x_t, x_ts
