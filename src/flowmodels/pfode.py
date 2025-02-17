2e1203870cacd2fb4466a846865fb9be6ab9426c
revsic <revsic99@gmail.com>
2025-02-17 10:32:28 +0900

    pfode: Split denoiser from solver

--- src/flowmodels/pfode.py
+++ src/flowmodels/pfode.py
@@ -17,23 +17,6 @@ class ProbabilityFlowODESampler(Sampler):
     def __init__(self, scheduler: Scheduler | ContinuousScheduler):
         self.scheduler = scheduler
 
-    def _discretized_var(self, steps: int | None = None) -> torch.Tensor:
-        """Discretize the variance sequence.
-        Args:
-            steps: the number of the steps.
-        Returns:
-            [FloatLike; [steps]], discretized variance sequence.
-        """
-        # discrete-time scheduler
-        if isinstance(self.scheduler, Scheduler):
-            assert steps is None or self.scheduler.T == steps
-            return self.scheduler.var()
-        # continuous-time scheduler
-        steps = steps or self.DEFAULT_STEPS
-        # [T], in range[1 / T, 1]
-        timesteps = torch.arange(1, steps + 1, dtype=torch.float32) / steps
-        return self.scheduler.var(timesteps)
-
     def sample(
         self,
         model: ScoreModel,
@@ -59,6 +42,61 @@ class ProbabilityFlowODESampler(Sampler):
         # variance exploding
         return self._solve_variance_exploding_score_model(model, prior, steps, verbose)
 
+    def _discretized_var(self, steps: int | None = None) -> torch.Tensor:
+        """Discretize the variance sequence.
+        Args:
+            steps: the number of the steps.
+        Returns:
+            [FloatLike; [steps]], discretized variance sequence.
+        """
+        # discrete-time scheduler
+        if isinstance(self.scheduler, Scheduler):
+            assert steps is None or self.scheduler.T == steps
+            return self.scheduler.var()
+        # continuous-time scheduler
+        steps = steps or self.DEFAULT_STEPS
+        # [T], in range[1 / T, 1]
+        timesteps = torch.arange(1, steps + 1, dtype=torch.float32) / steps
+        return self.scheduler.var(timesteps)
+
+    def _compute_vp_beta(self, alpha_bar: torch.Tensor) -> torch.Tensor:
+        """Compute the beta sequence.
+        Args:
+            alpha_bar: a list of the `1 - var_t` where `var_t` is discretized variances.
+        Returns:
+            a list of the betas.
+        """
+        (T,) = alpha_bar.shape
+        if isinstance(self.scheduler, VPSDEAncestralSamplerSupports):
+            _t = torch.arange(1, T + 1, dtype=torch.float32) / T
+            beta = self.scheduler.betas(_t) / T
+        else:
+            # [T], emperical estimation
+            beta = 1 - alpha_bar / F.pad(alpha_bar[:-1], [1, 0], "constant", 1.0)
+        return beta
+
+    def _denoise_vp(
+        self, model: ScoreModel, x_t: torch.Tensor, t: torch.Tensor, beta: torch.Tensor
+    ) -> torch.Tensor:
+        """Denoise the given sample `x_t` to the single-step backward `x_{t-1}`.
+        Args:
+            model: variance-preserving score model.
+            x_t: [FloatLike; [B, ...]], the given samples, `x_t`.
+            t: [torch.long; [B]], the current timesteps in range[1, T].
+            beta: [FloatLike; [T]], a list of betas.
+        Returns:
+            [FloatLike; [B, ...]], denoised sample, `x_{t-1}`.
+        """
+        # T
+        (T,) = beta.shape
+        # B
+        (bsize,) = t.shape
+        # [B, ...]
+        b = beta[t - 1].view([bsize] + [1] * (x_t.dim() - 1))
+        # [B, ...]
+        score = model.score(x_t, t / T)
+        return (2 - (1 - b).sqrt()).to(x_t) * x_t + 0.5 * b.to(x_t) * score
+
     def _solve_variance_preserving_score_model(
         self,
         model: ScoreModel,
@@ -83,23 +121,36 @@ class ProbabilityFlowODESampler(Sampler):
         # B
         bsize, *_ = prior.shape
         # [T]
-        alpha_bar = 1 - self._discretized_var(steps)
+        beta = self._compute_vp_beta(_alpha_bar := 1 - self._discretized_var(steps))
         # T
-        (T,) = alpha_bar.shape
-        if isinstance(self.scheduler, VPSDEAncestralSamplerSupports):
-            _t = torch.arange(1, T + 1, dtype=torch.float32) / T
-            beta = self.scheduler.betas(_t) / T
-        else:
-            # [T]
-            beta = 1 - alpha_bar / F.pad(alpha_bar[:-1], [1, 0], "constant", 1.0)
+        (T,) = beta.shape
         x_t, x_ts = prior, []
         with torch.inference_mode():
             for t in verbose(range(T, 0, -1)):
-                score = model.score(x_t, torch.full((bsize,), t / T))
-                b = beta[t - 1]
-                x_t = (2 - (1 - b).sqrt().to(x_t)) * x_t + 0.5 * b.to(x_t) * score
+                t = torch.full((bsize,), t, dtype=torch.long)
+                x_t = self._denoise_vp(model, x_t, t, beta)
                 x_ts.append(x_t)
         return x_t, x_ts
+
+    def _denoise_ve(
+        self, model: ScoreModel, x_t: torch.Tensor, t: torch.Tensor, var: torch.Tensor
+    ) -> torch.Tensor:
+        """Denoise the given sample `x_t` to the single-step backward `x_{t-1}`.
+        Args:
+            model: variance-exploding score model.
+            x_t: [FloatLike; [B, ...]], the given samples, `x_t`.
+            t: [torch.long; [B]], the current timesteps in range[1, T].
+            var: [FloatLike; [T]] a list of variances.
+        Returns:
+            [FloatLike; [B, ...]], denoised sample, `x_{t-1}`.
+        """
+        # T
+        (T,) = var.shape
+        # [T, ...]
+        var = var.view([T] + [1] * (x_t.dim() - 1))
+        # [B, ...]
+        score = model.score(x_t, t / T)
+        return x_t + 0.5 * (var[t - 1] - var[(t - 2).clamp_min(0)]).to(score) * score
 
     def _solve_variance_exploding_score_model(
         self,
@@ -132,8 +183,8 @@ class ProbabilityFlowODESampler(Sampler):
         (T,) = var.shape
         x_t, x_ts = prior, []
         with torch.inference_mode():
-            for t in verbose(range(T, 1, -1)):
-                score = model.score(x_t, torch.full((bsize,), t / T))
-                x_t = x_t + 0.5 * (var[t - 1] - var[t - 2]).to(score) * score
+            for t in verbose(range(T, 0, -1)):
+                t = torch.full((bsize,), t, dtype=torch.long)
+                x_t = self._denoise_ve(model, x_t, t, var)
                 x_ts.append(x_t)
         return x_t, x_ts
