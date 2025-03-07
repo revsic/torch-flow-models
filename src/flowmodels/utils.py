ba8ce9440fa4ee05674c298f53b2cfb90c727378
revsic <revsic99@gmail.com>
2025-03-07 14:24:27 +0900

    utils: Move EMASupports

--- src/flowmodels/utils.py
+++ src/flowmodels/utils.py
@@ -1,6 +1,31 @@
+import copy
+from typing import Self
+
 import torch
+import torch.nn as nn
 
 from flowmodels.basis import VelocitySupports
+
+
+class EMASupports[T: nn.Module](nn.Module):
+    def __init__(self, module: T):
+        super().__init__()
+        self.module = copy.deepcopy(module)
+
+    @torch.no_grad()
+    def update(self, module: nn.Module, mu: float | torch.Tensor):
+        given = dict(module.named_parameters())
+        for name, param in self.module.named_parameters():
+            assert name in given, f"parameters not found; named `{name}`"
+            param.copy_(mu * param.data + (1 - mu) * given[name].data)
+
+    @classmethod
+    def reduce(cls, self_: T, ema: T | Self | None = None) -> T:
+        if ema is None:
+            return self_
+        if isinstance(ema, EMASupports):
+            return ema.module
+        return ema
 
 
 class VelocityInverter(VelocitySupports):
