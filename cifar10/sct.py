3cb171dac9a0820afcf65e3d5c4a9888abd8ad2f
revsic <revsic99@gmail.com>
2025-03-18 06:29:25 +0000

    sct: Add tangent wramup

---
+++ cifar10/sct.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone, timedelta
+from pathlib import Path
+
+import torch
+from safetensors.torch import load_model
+
+from ddpmpp_cm import UNetModel
+from flowmodels.sct import ScaledContinuousCM, ScaledContinuousCMScheduler
+from trainer import Cifar10Trainer
+from trigflow import InverseSquareRootScheduler
+
+
+def reproduce_sct_cifar10():
+    # model definition
+    backbone = UNetModel(
+        in_channels=3,
+        model_channels=128,
+        out_channels=3,
+        num_res_blocks=4,
+        attention_resolutions=[16],
+        dropout=0.20,
+        channel_mult=[1, 2, 2, 2],
+        conv_resample=True,  # Unknown
+        num_heads=1,
+        use_scale_shift_norm=True,
+        use_double_norm=True,
+        resblock_updown=False,
+        temb_scale=0.02,
+        attn_module="legacy",
+        attn_dtype=torch.float16,
+    )
+    model = ScaledContinuousCM(
+        backbone,
+        ScaledContinuousCMScheduler(),
+        tangent_warmup=10000,
+    )
+    load_model(
+        model,
+        filename=Path(
+            "./test.workspace/trigflow-cifar10/2025.03.15KST22:04:04/ckpt/2040/model.safetensors"
+        ),
+    )
+
+    n_gpus = 2
+    # timestamp
+    stamp = datetime.now(timezone(timedelta(hours=9))).strftime("%Y.%m.%dKST%H:%M:%S")
+    trainer = Cifar10Trainer(
+        model,
+        batch_size=512 // n_gpus,
+        shuffle=True,
+        dataset_path=Path("./"),
+        workspace=Path(f"./test.workspace/sct-cifar10/{stamp}"),
+    )
+    trainer.optim = torch.optim.RAdam(
+        trainer.model.parameters(),
+        lr=0.0001,
+        betas=(0.9, 0.99),
+        eps=1e-8,
+    )
+    trainer.scheduler = InverseSquareRootScheduler(
+        trainer.optim,
+        0.0001,
+        t_ref=35000,
+    )
+
+    trainer.train(
+        total=400000 * n_gpus,
+        mixed_precision="no",
+        gradient_accumulation_steps=1,
+    )
+
+
+if __name__ == "__main__":
+    reproduce_sct_cifar10()
