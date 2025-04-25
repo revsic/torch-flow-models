sct: Log debug infos and fix adaptive weighting

src/flowmodels/cvsct.py
@@ -21,6 +21,13 @@ class ConstantVelocityConsistencyModels(
         self.p_std = p_std
         self._ada_weight = _AdaptiveWeights()
         self.solver = VanillaEulerSolver()
+        # debug purpose
+        self._debug_from_loss = {}
+
+    # debug purpose
+    @property
+    def _debug_purpose(self):
+        return {**self._debug_from_loss, **getattr(self.F0, "_debug_purpose", {})}
 
     def forward(self, x_t: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         """Estimate the `x_0` from the given `x_t`.
@@ -94,15 +101,21 @@ class ConstantVelocityConsistencyModels(
         # reducing dimension
         rdim = [i + 1 for i in range(x_t.dim() - 1)]
         # normalized tangent
-        normalized_tangent = grad / (grad.norm(p=2, dim=rdim, keepdim=True) + 0.1)
+        normalized_tangent = grad / (_norm := grad.norm(p=2, dim=rdim, keepdim=True) + 0.1)
         # [B, ...]
         estim: torch.Tensor = self.F0.forward(x_t, t)
         # [B]
         mse = (estim - F - normalized_tangent).square().mean(dim=rdim)
         # [B], adaptive weighting
-        logvar = self._ada_weight.forward(_t).squeeze(dim=-1)
+        logvar = self._ada_weight.forward(t)
         # [B]
         loss = mse * logvar.exp() - logvar
+        with torch.no_grad():
+            self._debug_from_loss = {
+                "sct/mse": mse.mean().item(),
+                "sct/logvar": logvar.mean().item(),
+                "sct/tangent-norm": _norm.mean().item(),
+            }
         # []
         return loss.mean()
 